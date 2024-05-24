7feb7a15877e956754b17bdadd51e7230e15f1e7
woxane <mohsen.minavand1384@gmail.com>
2024-05-24 13:17:13 +0330

    Implement accessResource in Operator

--- src/main/java/sbu/cs/Semaphore/Operator.java
+++ src/main/java/sbu/cs/Semaphore/Operator.java
@@ -1,21 +1,37 @@
 package sbu.cs.Semaphore;
 
-public class Operator extends Thread {
+import java.util.concurrent.Semaphore;
 
-    public Operator(String name) {
+public class Operator extends Thread {
+    private Semaphore semaphore;
+
+    public Operator(String name, Semaphore semaphore) {
         super(name);
+        this.semaphore = semaphore;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++)
         {
-            Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
+            accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
             try {
                 sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void accessResource() {
+        try {
+            semaphore.acquire();
+            System.out.println("The " + "`" + Thread.currentThread().getName() + "`" + " At " + System.currentTimeMillis() + " is accessing the resource .");
+            Resource.accessResource();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
         }
     }
 }
